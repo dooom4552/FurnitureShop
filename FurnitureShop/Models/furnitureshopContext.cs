a7cd834b4d0dc0c3225a821feae4dacfac7a13eb
Рита Миннекаева <RitaRita1957@hotmail.com>
2020-07-30 17:22:34 +0300

    Добавьте файлы проекта.

---
+++ FurnitureShop/Models/furnitureshopContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FurnitureShop.Models
+{
+    public partial class furnitureshopContext : DbContext
+    {
+        public furnitureshopContext()
+        {
+        }
+
+        public furnitureshopContext(DbContextOptions<furnitureshopContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Food> Food { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=127.0.0.1;database=furnitureshop;uid=root;pwd=Ritasasha1885", x => x.ServerVersion("8.0.19-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Food>(entity =>
+            {
+                entity.ToTable("food");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ FurnitureShop/RoleInitializer.cs
@@ -0,0 +1,55 @@
+﻿using FurnitureShop.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FurnitureShop
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "Admin@";
+            string password = "Trader25@";
+            string governmentEmail = "Government@";
+            string governmentPassword = "Trader24@";
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+            if (await roleManager.FindByNameAsync("учреждение") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("учреждение"));
+            }
+            if (await roleManager.FindByNameAsync("управление") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("управление"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+
+            if (await userManager.FindByNameAsync(governmentEmail) == null)
+            {
+                User government = new User { Email = governmentEmail, UserName = governmentEmail };
+                IdentityResult result = await userManager.CreateAsync(government, governmentPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(government, "управление");
+                }
+            }
+        }
+    }
+}
